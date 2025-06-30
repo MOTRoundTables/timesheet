3869e36ef996ce0dfcd2cc10def0a467ecf9ee0d
g-bd <golanbendor@yahoo.com>
2025-06-30 11:31:21 +0300

    config example file

---
+++ config_example.py
@@ -0,0 +1,5 @@
+# change comfig_example to config.py
+username = "your_id_here"
+password = "your_code_here"
+# your excel here in the following format "C:\...\file.xlsx"
+excel_file_path = r"C:\your_path\your_file.xlsx"
